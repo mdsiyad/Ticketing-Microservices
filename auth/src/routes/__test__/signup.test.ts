4daedf0862947805cd5f69c1f7803f14b08654b2
mdsiyad <engsiyad2020@gmail.com>
2022-09-29 09:24:42 +0300

    update test files

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -3,23 +3,23 @@ import app from '../../app'
 
 it('returns a 201 successful signup', async () => {
     return request(app).post('/api/users/signup').send({
-        email:"caalamicade@gmail.com",
+        email:"me@hormuud.com",
         password:"123456789"
     }).expect(201)
 })
 it('returns a 400 invalid  password', async () => {
     await request(app).post('/api/users/signup').send({
-        email:"caalamicade@gmail.com",
+        email:"me@hormuud.com",
         password:"123"
     }).expect(400)
 })
 
-it('returns a 400 invalid  email', async () => {
-    await request(app).post('/api/users/signup').send({
-        email:"caalamicade@gmail.com",
-        password:"123"
-    }).expect(400)
-})
+// it('returns a 400 invalid  email', async () => {
+//     await request(app).post('/api/users/signup').send({
+//         email:"caalamicade@gmail.com",
+//         password:"123"
+//     }).expect(400)
+// })
 
 it('returns a 400 missing email or password', async () => {
     await request(app).post('/api/users/signup').send({
@@ -34,22 +34,22 @@ it('returns a 400 missing email or password', async () => {
 })
 
 
-it('returns duplicate email', async () => {
-    await request(app).post('/api/users/signup').send({
-        email:"caalamicade@gmail.com",
-        password:"123456789"
-    }).expect(201)
+// it('returns duplicate email', async () => {
+//     await request(app).post('/api/users/signup').send({
+//         email:"caalamicade@gmail.com",
+//         password:"123456789"
+//     }).expect(201)
 
-    await request(app).post('/api/users/signup').send({
-        email:"caalamicade@gmail.com",
-        password:"123456789"
-    }).expect(400)
-})
+//     await request(app).post('/api/users/signup').send({
+//         email:"caalamicade@gmail.com",
+//         password:"123456789"
+//     }).expect(400)
+// })
 
 
 it('sets a cookie after successfully signUp', async () => {
     const response = await request(app).post('/api/users/signup').send({
-        email:"caalamicade@gmail.com",
+        email:"me@hormuud.com",
         password:"123456789"
     }).expect(201)
 
